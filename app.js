600d48c9b307ad2543ad5cf1afb5e35a79a4bfb6
Alan TS <alanedgardo971@gmail.com>
2025-03-18 14:14:24 -0600

    Implementando la funcion de actualizacion para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let nombres = [];
 let input = document.getElementById('amigo');
+let lista = document.getElementById('listaAmigos');
 
 function agregarAmigo() {
   let nombre = input.value;
@@ -8,16 +9,38 @@ function agregarAmigo() {
     alert("Por favor, inserte un nombre.");
   } else {
     nombres.push(nombre);
-    input.value = '';
+    limpiarInput();
+    //document.getElementById("listaAmigos").innerHTML += "<li>" + nombre + "</li>";
+    MostrarLista();
   }
 
-
   /*
-  alert(`El nombre ingresado fue: ${nombre} ` + typeof (nombre))
+  alert(`El nombre ingresado fue: ${nombre} ` + typeof (nombre));
 
   for (let i = 0; i < nombres.length; i++) {
     const element = nombres[i];
     alert(element);
   }
     */
+}
+
+function limpiarInput() {
+  input.value = '';
+}
+
+
+function MostrarLista() {
+  let nombre = input.value.trim();
+  lista.innerHTML = "";
+
+  for (let i = 0; i < nombres.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = nombres[i];
+    lista.appendChild(li);
+  }
+}
+
+
+function sortearAmigo() {
+
 }
